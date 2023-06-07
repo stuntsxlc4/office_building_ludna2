ba43bb5a1d7a9fcd03433bacac65200485d3ba12
rafal <stuntsxlc4@gmail.com>
2023-06-07 21:00:52 +0200

    add menu and layout

---
+++ src/pages/Amenities.js
@@ -0,0 +1,11 @@
+import InfoPage from "../components/InfoPage";
+
+const Amenities = () => {
+    return <InfoPage>
+        <>
+            Amenities
+        </>
+    </InfoPage>
+}
+
+export default Amenities
---
+++ src/pages/Location.js
@@ -0,0 +1,11 @@
+import InfoPage from "../components/InfoPage";
+
+const Location = () => {
+    return <InfoPage>
+        <>
+            Location
+        </>
+    </InfoPage>
+}
+
+export default Location
