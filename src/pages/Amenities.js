efac784baab75faae7a379d68ac131eda6b520f0
rafal <stuntsxlc4@gmail.com>
2023-06-10 20:19:17 +0200

    add descriptions to office, modify structure

--- src/pages/Amenities.js
+++ src/pages/Amenities.js
@@ -3,7 +3,16 @@ import InfoPage from "../components/InfoPage";
 const Amenities = () => {
     return <InfoPage>
         <>
-            Amenities
+            <h1>Udogodnienia</h1>
+            <ul>
+                <li>Kawiarnia i przestrzenie co-workingowe</li>
+                <li>Restauracja</li>
+                <li>Taras na 40 piętrze do dyspozycji najemcy</li>
+                <li>Udogodnienia dla rowerzystów: szafki, prysznice, parking</li>
+                <li>Ładowarki dla samochodów elektrycznych i skuterów</li>
+            </ul>
+
+
         </>
     </InfoPage>
 }
--- src/pages/Office.js
+++ src/pages/Office.js
@@ -4,6 +4,16 @@ const Office = () => {
 
     return <InfoPage>
         <>
+            <h1>Offices</h1>
+            <p>The Bridge oferuje 47 000 m2 powierzchni przyjaznej dla każdego biznesu. Otwarta przestrzeń pozwala na szereg możliwości aranżacji biura.</p>
+
+            <ol>
+                <li>174 m wysokości</li>
+                <li>47000 m&sup2;</li>
+                <li>42 piętra naziemne</li>
+                <li>4 piętra podziemne </li>
+                <li>Windy double deck</li>
+            </ol>
 
         </>
     </InfoPage>
